56d503c48be5d96625bf1b21343d009d13f96151
Rom555 <romanzubrilin@gmail.com>
2022-03-28 13:43:26 +0300

    add table render

--- src/admin.js
+++ src/admin.js
@@ -1,5 +1,7 @@
 import { login } from './modules/admin/login';
 import { checkCookie } from './modules/admin/checkCookie';
+import { ItemService } from './modules/admin/ItemService';
+import { render } from './modules/admin/render';
 
 if (/\w*\.html/.exec(document.location.href)[0] === 'index.html') {
   checkCookie().then((success) => {
@@ -14,5 +16,11 @@ if (/\w*\.html/.exec(document.location.href)[0] === 'index.html') {
     if (!success) {
       window.location.replace('index.html');
     }
+  });
+
+  window.itemService = new ItemService('http://localhost:4545/prices');
+
+  itemService.getItems().then((data) => {
+    render(data);
   });
 }
---
+++ src/modules/admin/ItemService.js
@@ -0,0 +1,54 @@
+import { getData, sendData } from '../helper';
+
+export class ItemService {
+  constructor(url) {
+    this._url = url;
+  }
+
+  getItems() {
+    return getData(this._url);
+  }
+
+  addItem(item) {
+    return sendData(this._url, {
+      method: 'POST',
+      body: item,
+    });
+  }
+
+  removeItem(id) {
+    return sendData(`${this._url}/${id}`, {
+      method: 'DELETE',
+    });
+  }
+
+  changeItem(id, data) {
+    return sendData(`${this._url}/${id}`, {
+      method: 'PATCH',
+      body: data,
+    });
+  }
+
+  getItem(id) {
+    return getData(`${this._url}/${id}`);
+  }
+
+  editItem(id, item) {
+    return sendData(`${this._url}/${id}`, {
+      method: 'PUT',
+      body: item,
+    });
+  }
+
+  filterItems(filterOption) {
+    return getData(`${this._url}?${filterOption}=true`);
+  }
+
+  getSortItems(sortOption) {
+    return getData(`${this._url}?_sort=${sortOption.name}&_order=${sortOption.value}`);
+  }
+
+  getSearchItems(str) {
+    return getData(`${this._url}?name_like=${str}`);
+  }
+}
